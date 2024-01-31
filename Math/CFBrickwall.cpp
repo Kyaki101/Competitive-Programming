3be14e099d8049e42e6bf047a0d19c3360b2ffb9
kyaki <emanuelrs101@gmail.com>
2024-01-30 22:49:44 -0600

    Completed codeForces propblem a of contest

---
+++ Math/CFBrickwall.cpp
@@ -0,0 +1,31 @@
+/*
+https://codeforces.com/contest/1918/problem/A
+*/
+
+#include <bits/stdc++.h>
+
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    ll n;
+    ll m;
+    cin >> n >> m;
+    cout << (m / 2) * n << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    cin >> t;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
