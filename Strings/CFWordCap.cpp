a0bed8c73a076592d904b3a01fcae501d6a5ddb1
kyaki <emanuelrs101@gmail.com>
2024-02-09 23:50:19 -0600

    Completed code Forces problem

---
+++ Strings/CFWordCap.cpp
@@ -0,0 +1,31 @@
+/*
+https://codeforces.com/problemset/problem/281/A
+*/
+
+#include <bits/stdc++.h>
+
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    t = 1;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
