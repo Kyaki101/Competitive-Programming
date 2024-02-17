7bf5260bc32732ff581dbc2e2e36d19b4f303033
kyaki <emanuelrs101@gmail.com>
2024-02-16 23:10:00 -0600

    Completed bear and big bro cf

---
+++ Math/CFBearAndBigBrother.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    int a, b;
+    cin >> a >> b;
+    int res = 0;
+    while(a <= b){
+        res ++;
+        a *= 3;
+        b *= 2;
+    }
+    cout << res << endl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    t = 1;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
