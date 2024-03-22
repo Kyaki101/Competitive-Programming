cbfacc510b6be389780f47c85179ef468306b362
kyaki <emanuelrs101@gmail.com>
2024-03-21 23:52:33 -0600

    Completed codeforces problem C

---
+++ Strings/CFYesOrYes.cpp
@@ -0,0 +1,34 @@
+/*
+https://codeforces.com/problemset/problem/1703/A
+*/
+
+#include <bits/stdc++.h>
+
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    string s;
+    cin >> s;
+    if((s[0] == 'Y'  || s[0] == 'y') && (s[1] == 'E' || s[1] == 'e') && (s[2] == 'S' || s[2] == 's')){
+        cout << "YES\n";
+        return;
+    }
+    cout << "NO\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    cin >> t;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
