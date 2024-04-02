f770b6ea8b3e6765689941fe4a7aad4b93109a42
kyaki <emanuelrs101@gmail.com>
2024-04-01 21:34:16 -0600

    Completed codeForces problem

---
+++ Algorithms/Knapsack.cpp
@@ -0,0 +1,4 @@
+//Given a sset of objcets which have both a value and a weight (v_i, w_i) what is the maximium value
+//we can obtain by selecting a subset of heses objects such that the sum of the weights does not 
+//exceed a certain capacity
+
---
+++ Math/CFAgainTwenty.cpp
@@ -0,0 +1,33 @@
+/*
+*https://codeforces.com/problemset/problem/630/A
+*/
+
+
+#include <bits/stdc++.h>
+
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    int n;
+    cin >> n;
+    cout << 25 << endl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    t = 1;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
+
