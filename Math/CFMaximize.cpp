ad06c5b2943dc9b2346ad35635f170fa035c32ce
kyaki <emanuelrs101@gmail.com>
2024-05-03 23:50:30 -0600

    completed codeforces problem

---
+++ Math/CFMaximize.cpp
@@ -0,0 +1,34 @@
+/*
+*https://codeforces.com/contest/1968/problem/A
+*/
+
+
+
+#include <bits/stdc++.h>
+
+#define all(n) begin(n), end(n)
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    int n;
+    cin >> n;
+    cout << n - 1 << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    cin >> t;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
+
