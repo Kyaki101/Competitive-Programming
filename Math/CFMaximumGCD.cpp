9c32863f97d04c8879ff794a24338817c28f8121
kyaki <emanuelrs101@gmail.com>
2024-05-12 22:46:04 -0600

    completed codeforces problem

---
+++ Math/CFMaximumGCD.cpp
@@ -0,0 +1,33 @@
+/*
+*https://codeforces.com/problemset/problem/1370/A
+*/
+
+
+
+#include <bits/stdc++.h>
+
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    int n;
+    cin >> n;
+    cout << n / 2 << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    cin >> t;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
+
