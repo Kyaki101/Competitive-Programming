18dafedc74a75cc8165900073c73dd83ab5275df
kyaki <emanuelrs101@gmail.com>
2024-06-13 08:47:57 -0600

    completed codeforces problem

---
+++ Math/CFMaximumMultipleSum.cpp
@@ -0,0 +1,36 @@
+/*
+*https://codeforces.com/contest/1985/problem/B
+*/
+
+
+#include <bits/stdc++.h>
+
+
+typedef long long int ll;
+typedef std::vector<int> vec;
+using namespace std;
+
+void sol(){
+    int n;
+    cin >> n;
+    if(n == 3){
+        cout << 3 << endl;
+        return;
+    }
+    cout << 2 << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+              
+    int t;
+    cin >> t;
+    while(t--){
+        sol();
+    }
+
+
+    return 0;
+}
+
